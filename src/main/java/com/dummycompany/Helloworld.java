63d809b12942a674f5cb9953235599dbf1d36711
yasincebeci <cebeci.ysn@gmail.com>
2020-02-08 15:20:18 +0300

    first commit

---
+++ src/main/java/com/dummycompany/Helloworld.java
@@ -0,0 +1,9 @@
+package com.dummycompany;
+
+public class Helloworld {
+	public static void main(String args[])
+	{
+		System.out.println("Hello World!!");
+	}
+	
+}
---
+++ src/test/java/com/dummycompany/HelloworldTest.java
@@ -0,0 +1,14 @@
+package com.dummycompany;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloworldTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
